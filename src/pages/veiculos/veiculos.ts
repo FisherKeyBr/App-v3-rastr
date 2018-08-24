88e804a7dee43b1763330304a5711dbd5117a170
Gabriel Rodrigues <gabrielrodrigues@prodata.inf.br>
2018-08-23 21:20:28 -0300

    refactory na tela de veiculos..

--- src/pages/veiculos/veiculos.ts
+++ src/pages/veiculos/veiculos.ts
@@ -18,6 +18,7 @@ export class VeiculosPage {
   ALERTA_BATERIA: number = 64784;
   ALERTA_VELOCIDADE: number = 61722;
   ALERTA_PANICO: number = 63553;
+  ALERTA_EM_MOVIMENTO: number = 61714;
 
   constructor(
     public navCtrl: NavController,
@@ -46,13 +47,43 @@ export class VeiculosPage {
         return 'orange';
       case this.ALERTA_PANICO:
         return 'danger';
+      case this.ALERTA_EM_MOVIMENTO:
+        return 'green';
       default:
         return 'light';
     }
   }
 
+  getDescricaoAlerta(historico){
+    if (!historico) {
+      return '';
+    }
+
+    switch (historico.StatusCode) {
+      case this.ALERTA_BATERIA:
+        return 'Status: Bateria baixa';
+      case this.ALERTA_VELOCIDADE:
+        return 'Status: Velocidade ultrapassada';
+      case this.ALERTA_PANICO:
+        return 'Status: Panico';
+      case this.ALERTA_EM_MOVIMENTO:
+        return 'Status: Em movimento';
+      default:
+        return '';
+    }
+  }
+
   getIndexUltimoHistorico(veiculo: Veiculo) {
     return veiculo.EventData && veiculo.EventData.length > 0 ? veiculo.EventData.length - 1 : 0;
+  }
+
+  atualizarVeiculos(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
   }
 
   executarChamadasInicial() {
