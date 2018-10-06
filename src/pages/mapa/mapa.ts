3b60ba55d742757e8f842ea3cb632e04bceac29c
Gabriel Rodrigues <gabriel.dev30@gmail.com>
2018-10-06 19:26:54 -0300

    openstreet maps implementado..

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -5,16 +5,23 @@ import {VeiculoService} from "../../services/veiculo-service";
 import {LoginPage} from "../login/login";
 import {LoginService} from "../../services/login-service";
 import {AlertService} from "../../services/alert-service";
+import {AlertaVeiculoEnum} from "../../enums/alerta-veiculo";
+import {Observable} from 'Rxjs/rx';
+import {Subscription} from "rxjs/Subscription";
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'page-mapa',
   templateUrl: 'mapa.html',
 })
 export class MapaPage {
-
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  Osmap: any;
   historicos: any;
+  tipoMapa: string;
+  batteryOSControl: any;
+  refreshHistoryFunc: Subscription;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -23,10 +30,19 @@ export class MapaPage {
               public alertService: AlertService,
               public loadingCtrl: LoadingController) {
     this.historicos = [];
+    this.tipoMapa = 'google';
   }
 
   ionViewDidEnter() {
     this.loadMap();
+
+    this.refreshHistoryFunc = Observable.interval(45000).subscribe(() => {
+      this.loadMap();
+    });
+  }
+
+  ionViewDidLeave(){
+    this.refreshHistoryFunc.unsubscribe();
   }
 
   ionViewWillEnter() {
@@ -45,7 +61,13 @@ export class MapaPage {
 
     this.veiculoService.getHistoricoVeiculo(this.navParams.get('deviceId')).subscribe((retorno) => {
       this.historicos = retorno || [];
-      this.addMarkersMap();
+
+      if (this.tipoMapa === 'google') {
+        this.addGoogleMarkersMap();
+      } else {
+        this.addOpenStreetMarkersMap();
+      }
+
       loading.dismiss();
     }, (error) => {
       this.alertService.showError(error.message);
@@ -53,7 +75,105 @@ export class MapaPage {
     });
   }
 
-  addMarkersMap() {
+  /**
+   * The CenterControl adds a control to the map that recenters the map on
+   * Chicago.
+   * This constructor takes the control DIV as an argument.
+   * @constructor
+   */
+  placeBatteryComponentGoogleMap(controlDiv, historico) {
+    let batteryImg = '';
+
+    if (parseInt(historico.batteryLevel) <= 0) {
+      batteryImg = '../../assets/icon/battery_min.png';
+    } else {
+      batteryImg = '../../assets/icon/battery_max.png';
+    }
+
+    // Set CSS for the control border.
+    let controlUI = document.createElement('img');
+    controlUI.src = batteryImg;
+    controlUI.style.marginBottom = '22px';
+    controlDiv.appendChild(controlUI);
+  }
+
+  placeBatteryComponentOSMap(historico) {
+    let batteryImg = '';
+
+    if (parseInt(historico.batteryLevel) <= 0) {
+      batteryImg = '../../assets/icon/battery_min.png';
+    } else {
+      batteryImg = '../../assets/icon/battery_max.png';
+    }
+
+    //logo position: bottomright, topright, topleft, bottomleft
+    this.batteryOSControl = leaflet.control({position: 'bottomleft'});
+
+    this.batteryOSControl.onAdd = function () {
+      var div = leaflet.DomUtil.create('div', 'os-battery');
+      div.innerHTML = "<img src='" + batteryImg + "'>";
+      return div;
+    };
+
+    this.batteryOSControl.addTo(this.Osmap);
+  }
+
+  addOpenStreetMarkersMap() {
+    let latitude = -23.533773, longitude = -46.625290;
+
+    if (!!this.historicos && this.historicos.length > 0) {
+      latitude = this.historicos[0].latitude;
+      longitude = this.historicos[0].longitude;
+    }
+
+    if(this.Osmap){
+      this.Osmap.off();
+      this.Osmap.remove();
+    }
+
+    this.Osmap = leaflet.map('osmap', {
+      center: [latitude, longitude],
+      zoom: 16
+    });
+
+    leaflet.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet'
+    }).addTo(this.Osmap);
+
+    var icon = leaflet.icon({
+      iconUrl: '../../assets/icon/car.png'
+    });
+
+    this.placeBatteryComponentOSMap(this.historicos[0]);
+
+    for (var x = 0, y = this.historicos.length; x < y; x++) {
+      let historico: any = this.historicos[x];
+      this.setOSWindowMarkerInfo(historico, icon);
+    }
+  }
+
+  setOSWindowMarkerInfo(historico, icon) {
+    var marker = new leaflet.Marker([historico.latitude, historico.longitude], {icon: icon});
+    this.Osmap.addLayer(marker);
+
+    let ignitionImg = '';
+
+    if (historico.statusCode === AlertaVeiculoEnum.IGNICAO_LIGADA) {
+      ignitionImg = '<b>Ignição: </b><br>' + '<img style="background-color: transparent" src="../../assets/icon/ignicao_ligada.jpeg">';
+    } else {
+      ignitionImg = '<b>Ignição: </b><br>' + '<img style="background-color: transparent" src="../../assets/icon/ignicao_desligada.jpeg">';
+    }
+
+    marker.bindPopup('<div>' +
+      '<b>Endereço: </b> ' + historico.address + '<br>' +
+      '<b>Odómetro: </b>' + (historico.odometerKM || 0).toFixed(2) + '/KM<br>' +
+      '<b>Última conexão: </b>' + historico.ultima_conexao + '<br>' +
+      '<b>Velocidade: </b>' + historico.speedKPH + '/KPH <br>' +
+      ignitionImg + '<br>' +
+      '</div>', {maxWidth: 200}).openPopup();
+  }
+
+  addGoogleMarkersMap() {
     let latitude = -23.533773, longitude = -46.625290;
 
     if (!!this.historicos && this.historicos.length > 0) {
@@ -73,41 +193,52 @@ export class MapaPage {
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
+    let centerControlDiv: any = document.createElement('div');
+    this.placeBatteryComponentGoogleMap(centerControlDiv, this.historicos[0]);
+
+    centerControlDiv.index = 1;
+    this.map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(centerControlDiv);
+
     for (var x = 0, y = this.historicos.length; x < y; x++) {
       let historico: any = this.historicos[x];
-      let latLng = new google.maps.LatLng(historico.latitude, historico.longitude);
-
-      let marker: google.maps.Marker = new google.maps.Marker({
-        animation: google.maps.Animation.DROP,
-        position: latLng,
-        icon: (historico.icon || '../../assets/icon/pin.png')
-      });
-
-      let batteryImg = '';
-
-      if (parseInt(historico.batteryLevel) <= 0) {
-        batteryImg = '<img style="float: right" src="../../assets/icon/battery_min.png">';
-      } else {
-        batteryImg = '<img style="float: right" src="../../assets/icon/battery_max.png">';
-      }
-
-      var infowindow = new google.maps.InfoWindow({
-        content: '<div>' +
-          '<b>Endereço: </b> ' + historico.address + '<br>' +
-          '<b>Odómetro: </b>' + (historico.odometerKM || 0).toFixed(2) + '/KM<br>' +
-          '<b>Última conexão: </b>' + historico.ultima_conexao + '<br>' +
-          '<b>Velocidade: </b>' + historico.speedKPH + '/KPH ' + batteryImg + '<br>' +
-          '</div>',
-        maxWidth: 200
-      });
-
-      marker.addListener('click', () => {
-        infowindow.open(this.map, marker);
-      });
-
-      marker.setMap(this.map);
-      infowindow.open(this.map, marker);
+      this.setGoogleWindowMarkerInfo(historico);
     }
+  }
+
+  setGoogleWindowMarkerInfo(historico) {
+    let latLng = new google.maps.LatLng(historico.latitude, historico.longitude);
+
+    let marker: google.maps.Marker = new google.maps.Marker({
+      animation: google.maps.Animation.DROP,
+      position: latLng,
+      icon: (historico.icon || '../../assets/icon/pin.png')
+    });
+
+    let ignitionImg = '';
+
+    if (historico.statusCode === AlertaVeiculoEnum.IGNICAO_LIGADA) {
+      ignitionImg = '<b>Ignição: </b><br>' + '<img style="background-color: transparent" src="../../assets/icon/ignicao_ligada.jpeg">';
+    } else {
+      ignitionImg = '<b>Ignição: </b><br>' + '<img style="background-color: transparent" src="../../assets/icon/ignicao_desligada.jpeg">';
+    }
+
+    var infowindow = new google.maps.InfoWindow({
+      content: '<div>' +
+        '<b>Endereço: </b> ' + historico.address + '<br>' +
+        '<b>Odómetro: </b>' + (historico.odometerKM || 0).toFixed(2) + '/KM<br>' +
+        '<b>Última conexão: </b>' + historico.ultima_conexao + '<br>' +
+        '<b>Velocidade: </b>' + historico.speedKPH + '/KPH <br>' +
+        ignitionImg + '<br>' +
+        '</div>',
+      maxWidth: 200
+    });
+
+    marker.addListener('click', () => {
+      infowindow.open(this.map, marker);
+    });
+
+    marker.setMap(this.map);
+    infowindow.open(this.map, marker);
   }
 }
 
